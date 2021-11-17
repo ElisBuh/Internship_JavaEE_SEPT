a6963455d9120b63a06f7b47ca2188cbd6dbc886
Elis <Byhovtsov@gmail.com>
2021-11-17 11:32:20 +0300

    HW06: Task 1.2 ready

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,10 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -12,6 +16,9 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.TimingRules;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
@@ -24,12 +31,33 @@ import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
+    protected static String[] activeProfiles;
+
+    @Autowired
+    protected Environment environment;
+
+    protected JpaUtil jpaUtil;
+
+    @Autowired
+    protected ApplicationContext applicationContext;
+
+    @Before
+    public void setupProfiles(){
+        activeProfiles = environment.getActiveProfiles();
+        if (!Arrays.asList(activeProfiles).contains("jdbc")) {
+            jpaUtil = applicationContext.getBean(JpaUtil.class);
+        }
+    }
 
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
 
     @Rule
     public Stopwatch stopwatch = TimingRules.STOPWATCH;
+
+
+
+
 
     //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
     protected <T extends Throwable> void validateRootCause(Class<T> rootExceptionClass, Runnable runnable) {
